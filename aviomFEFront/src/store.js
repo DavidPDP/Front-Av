ed29d1cb104a696a92a9c56f30e66bb5cd4d5add
mgltorsa <mgltorsa@gmail.com>
2020-06-27 16:00:43 -0500

    added parameters module at store

--- aviomFEFront/src/store.js
+++ aviomFEFront/src/store.js
@@ -3,6 +3,7 @@ import Vuex from 'vuex';
 import createPersistedState from "vuex-persistedstate";
 import operators from "@/store/operators.module";
 import events from "@/store/events.module";
+import evalParameters from "@/store/evaluator.parameters.module";
 
 Vue.use(Vuex);
 
@@ -20,7 +21,8 @@ const getDefaultState=()=>{
 export default new Vuex.Store({
   modules: {
     operators,
-    events
+    events,
+    evalParameters
   },
   plugins:[createPersistedState()],
   state: {
