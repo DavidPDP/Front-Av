b67692a094f39e8a45f28ec072ab1f8ed55ae945
mgltorsa <mgltorsa@gmail.com>
2020-06-27 13:18:21 -0500

    added dashboard module an its actions

--- aviomFEFront/src/store/actions.type.js
+++ aviomFEFront/src/store/actions.type.js
@@ -1,8 +1,20 @@
 //Operators module
-export const FETCH_OPERATORS="fetchOperators";
-export const FETCH_CONTROLLERS="fetchControllers";
-export const FETCH_OPERATOR_HISTORY="fecthOperatorHistory";
+export const FETCH_OPERATORS = "fetchOperators";
+export const FETCH_CONTROLLERS = "fetchControllers";
+export const FETCH_OPERATOR_HISTORY = "fecthOperatorHistory";
 
 //Events module
-export const FETCH_EVENTS="fetchEvents";
-export const FETCH_EVENT_HISTORY="fecthEventHistory";
+export const FETCH_EVENTS = "fetchEvents";
+export const FETCH_EVENT_HISTORY = "fecthEventHistory";
+
+//Dashboard
+export const FETCH_KPI = "fetchKPI";
+
+//Expressions
+export const FETCH_VARIABLE = "fetchVariables";
+export const SAVE_VARIABLE = "saveVariable";
+export const UPDATE_VARIABLE = "updateVariable";
+export const FETCH_FUNCTIONS = "fetchFunctions";
+export const EVALUATE_EXPRESSION = "evaluateExpression"
+
+//Parameters
